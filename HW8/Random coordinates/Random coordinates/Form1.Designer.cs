f1612bbbcd19771183eb20ca9199207454bbf8af
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2023-02-06 18:01:30 +0100

    Add files via upload

---
+++ HW8/Random coordinates/Random coordinates/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Random_coordinates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 203);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 381);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(610, 587);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox3, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(413, 203);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(194, 381);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.tableLayoutPanel1.SetRowSpan(this.pictureBox2, 3);
+            this.pictureBox2.Size = new System.Drawing.Size(404, 194);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(488, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "5000";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(413, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
+            this.button1.Location = new System.Drawing.Point(413, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HW8/Random coordinates/Random coordinates/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_coordinates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Global.TOTAL = Int32.Parse(textBox1.Text);
+
+            Chart chart = new Chart(pictureBox1.Width, pictureBox1.Height, 1000, 1000, pictureBox1);
+            Chart chartXDistribution = new Chart(pictureBox2.Width, pictureBox2.Height, 0, 0, pictureBox2);
+            Chart chartYDistribution = new Chart(pictureBox3.Width, pictureBox3.Height, 0, 0, pictureBox3);
+
+            PointDistribution pointDistribution = new PointDistribution(Global.TOTAL, 500);
+            chart.InsertPoints(pointDistribution.PointCollection, 2, Color.Black);
+
+            chartXDistribution.InsertCandles(pointDistribution.EmpiricalDistributionX, pointDistribution.EmpiricalDistributionX.Count * 2, pointDistribution.MaxX, Color.Green);
+            chartYDistribution.InsertCandles(pointDistribution.EmpiricalDistributionY, pointDistribution.MaxY, pointDistribution.EmpiricalDistributionY.Count * 2, Color.Green, false);
+        }
+    }
+
+    public static class Global
+    {
+        public static int TOTAL = 10000;
+        public static int MIN_RADIANT = 1; 
+        public static int MAX_RADIANT = 628319;
+        public static int PRECISION_RAD = 100000;
+        public static int MAX_RADIUS = 500;
+
+        public static Random rng = new Random();
+    }
+
+    public static class RandomSelection
+    {
+        public static bool BoolProbability(double p)
+        {
+            double f = NextDouble(1, 100);
+            return f < p;
+        }
+
+        public static double NextDouble(double minimum, double maximum)
+        {
+            return Global.rng.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static List<T> SelectionSampling<T>(List<T> l, int n)
+        {
+            List<T> result = new List<T>();
+            for (int i = 0; i < l.Count; i++)
+            {
+                double p = ((double)(n - result.Count) / (double)(l.Count - i)) * 100;
+                if (BoolProbability(p)) { result.Add(l[i]); }
+            }
+            return result;
+        }
+    }
+
+    public class PointDistribution
+    {
+        public List<(double, double)> PointCollection { get; set; }
+        public List<(int, int)> EmpiricalDistributionX { get; set; }
+        public List<(int, int)> EmpiricalDistributionY { get; set; }
+        public int MaxX = 0;
+        public int MaxY = 0;
+
+        public PointDistribution(int n, int offset)
+        {
+            PointCollection = new List<(double, double)>();
+            EmpiricalDistributionX = new List<(int, int)>();
+            EmpiricalDistributionY = new List<(int, int)>();            
+
+            List<int> empiricalDistributionXValue = new List<int>();
+            List<int> empiricalDistributionYValue = new List<int>();
+
+            for(int i=0; i<100; i++)
+            {
+                empiricalDistributionXValue.Add(0);
+                empiricalDistributionYValue.Add(0);
+            }
+
+            for (int i = 0; i < n; i++)
+            {                
+                double angle = (double)(Global.rng.Next(Global.MIN_RADIANT, Global.MAX_RADIANT)) / (double)Global.PRECISION_RAD;
+                int radius = Global.rng.Next(1, Global.MAX_RADIUS);
+
+                double x = ((double)radius * Math.Cos(angle)) + (double)offset;
+                double y = ((double)radius * Math.Sin(angle)) + (double)offset;
+
+                empiricalDistributionXValue[(int)x/(int)10] += 1;
+                empiricalDistributionYValue[(int)y/(int)10] += 1;
+                
+                PointCollection.Add((x, y));
+            }
+
+            for (int i=0; i<100; i++)
+            {
+                EmpiricalDistributionX.Add((i+1, empiricalDistributionXValue[i]));
+                EmpiricalDistributionY.Add((i+1, empiricalDistributionYValue[i]));
+
+                if(MaxX < empiricalDistributionXValue[i]) { MaxX = empiricalDistributionXValue[i]; }
+                if(MaxY < empiricalDistributionYValue[i]) { MaxY = empiricalDistributionYValue[i]; }
+
+                Debug.WriteLine(EmpiricalDistributionX[i]);
+            }
+        }
+    }
+
+    public class Chart
+    {
+        public Bitmap _Bitmap { get; private set; }
+        public Graphics _Graphics { get; private set; }
+        public PictureBox _PictureBox { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public int MinX { get; set; }
+        public int MinY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public (List<(int, int)>, Color) CandlesMemory;
+        public int YMemory;
+        public (string, (int, int)) TextMemory;
+
+        public Chart(int width, int height, int maxX, int maxY, PictureBox pictureBox)
+        {
+            _Bitmap = new Bitmap(width, height);
+            _Graphics = Graphics.FromImage(_Bitmap);
+            _PictureBox = pictureBox;
+
+            Width = width;
+            Height = height;
+
+            //_Graphics.DrawRectangle(new Pen(Color.Black, 1), 0, 0, Width - 1, Height - 1);
+
+            MaxX = maxX;
+            MaxY = maxY;
+            MinX = 0;
+            MinY = 0;
+
+            _PictureBox.Image = _Bitmap;
+        }
+
+        public void DrawLine(int x1, int y1, int x2, int y2, Pen pen)
+        {
+            _Graphics.DrawLine(pen, x1, y1, x2, y2);
+        }
+        public void DrawLine((int, int) p1, (int, int) p2, Pen pen)
+        {
+            _Graphics.DrawLine(pen, p1.Item1, p1.Item2, p2.Item1, p2.Item2);
+        }
+        public void DrawCandle(int x, int y, int thickness, Color color, bool vertical=true)
+        {
+            Pen pen = new Pen(color, thickness);
+            if (vertical) { DrawLine(ConvertCoordinates((x, 0)), ConvertCoordinates((x, y)), pen); }
+            else { DrawLine(ConvertCoordinates((0, x)), ConvertCoordinates((y, x)), pen); }
+        }
+        public void DrawFilledCircle((int, int) point, int radius, SolidBrush brush)
+        {
+            _Graphics.FillEllipse(brush, point.Item1, point.Item2, radius, radius);
+        }
+
+        public (int, int) ConvertCoordinates((int, int) point)
+        {
+            float x = ((float)(point.Item1 - MinX) / (float)(MaxX - MinX)) * Width;
+            float y = Height - (((float)(point.Item2 - MinY) / (float)(MaxY - MinY)) * Height);
+
+            return ((int)x, (int)y);
+        }
+        public (int, int) ConvertCoordinates((double, double) point)
+        {
+            float x = ((float)(point.Item1 - MinX) / (float)(MaxX - MinX)) * Width;
+            float y = Height - (((float)(point.Item2 - MinY) / (float)(MaxY - MinY)) * Height);
+
+            return ((int)x, (int)y);
+        }
+        public void InsertCandles(List<(int, int)> collection, int maxX, int maxY, Color color, bool vertical = true)
+        {
+            CandlesMemory = (collection, color);
+            int thickness;
+            if (vertical) { thickness = (Width / collection.Count) + 1; }
+            else { thickness = (Height / collection.Count) + 1; }
+            MaxX = maxX;
+            MaxY = maxY;
+
+            Debug.WriteLine(MaxX);
+            Debug.WriteLine(MaxY);
+
+            foreach ((int, int) item in collection) {
+                //Debug.WriteLine($"{(item.Item1 * 2)-1} {item.Item2}");
+                DrawCandle((item.Item1 * 2) - 1, item.Item2, thickness, color, vertical); 
+            }
+            _PictureBox.Image = _Bitmap;
+        }
+        public void InsertCollection(List<(int, int)> collection, Color color)
+        {
+            (int, int) buffer = collection[0];
+            Pen pen = new Pen(color, 1);
+
+            foreach ((int, int) item in collection.Skip(1))
+            {
+                DrawLine(ConvertCoordinates(buffer), ConvertCoordinates(item), pen);
+                buffer = item;
+            }
+            _PictureBox.Image = _Bitmap;
+        }
+        public void InsertPoints(List<(double, double)> collection , int radius, Color color)
+        {
+            SolidBrush brush = new SolidBrush(color);
+            foreach ((double, double) point in collection) { DrawFilledCircle(ConvertCoordinates(point), radius, brush); }
+        }
+
+        public void InsertYLine(int y)
+        {
+            YMemory = y;
+            DrawLine(ConvertCoordinates((0, y)), ConvertCoordinates((MaxX, y)), new Pen(Color.Red, 2));
+        }
+        public void InsertText(string text, (int, int) coordinates)
+        {
+            TextMemory = (text, coordinates);
+            using (Font myFont = new Font("Calibri", 12))
+            {
+                (int, int) point = ConvertCoordinates(coordinates);
+                _Graphics.DrawString(text, myFont, Brushes.Red, new Point(point.Item1, point.Item2));
+            }
+            _PictureBox.Image = _Bitmap;
+        }
+    }
+}
